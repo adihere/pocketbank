be2aeb8758db0d88b52167c51c04197369c70d0c
adi <adihere@users.noreply.github.com>
2023-02-18 10:47:55 +0000

    TEMP COMMENT

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/**  TEMP COMMENT 
 const express = require('express');//Set up the express module
 const app = express();
 const router = express.Router();
@@ -11,3 +12,5 @@ const db = new Database()
 db.set("balance", 0).then(() => {});
 
 db.get("balance").then(value => {});
+
+ */
