e7d56e3a859618e9a1ff17d8ea236f83e569caf6
adi <adihere@users.noreply.github.com>
2023-02-22 21:09:09 +0000

    tnkr

--- src/server.js
+++ src/server.js
@@ -10,13 +10,49 @@ var cors = require('cors');
 app.use(cors());
 
 // keep alive  - express
-var server = app.listen(port+1);
+var server = app.listen(port + 1);
 server.keepAliveTimeout = 30000;
 
 const balanceURL = 'https://pocketbank.adityavadaganadam.repl.co/bal';
 
-app.get('/', (req, res) => {
-  res.send('Pocket Money - Bank of AV')
+app.get('/', async (req, res) => {
+  let balance = await db.get("balance");
+  let respmsg = 'Pocket Money - Bank of AV';
+  
+  if (typeof balance === undefined || balance == null) {
+    respmsg += ' - Your bank balance is empty, add some money!';  
+  } else {
+      respmsg += ' your balance is ' + balance;    
+  }
+  
+  res.send( respmsg);
+})
+
+
+// subs from balance  - custom amount
+app.put('/spent', async (req, res) => {
+
+  var respmsg ='default';
+  var spent = 0;
+  
+  if ( req === null || req === undefined || req.body === undefined || req.body === null  ) {
+     respmsg = 'request is null or undefined';
+     process.exit(4);
+  }
+  else {    
+    spent = parseInt(req.body.amount);
+    respmsg = 'Request is present and amount is ' + spent;    
+  }
+  
+  res.send('From server - PUT request to add:' + spent);
+  let balance = await db.get("balance");
+  //balancenum = parseInt(balance);
+  if (isNaN(balance)) { return 0; }
+  
+  console.log('change by' + spent);
+  balance = balance - spent;
+  await db.set("balance", balance);
+
 })
 
 
@@ -47,22 +83,9 @@ app.put('/sub10', async (req, res) => {
   let balance = await db.get("balance");
   balance -= 10;
   await db.set("balance", balance);
-  
+
 })
-
-
-// subs from balance - POST method 
-app.post('/spent', async (req, res) => {
-  res.send('Got a PUT request');
-  let balance = await db.get("balance");
-  balance -=  req.amount;
-  await db.set("balance", balance);
-  
-})
-
-
 
 app.listen(port, () => {
-   
   console.log(`Example app listening on port ${port}`)
 })
